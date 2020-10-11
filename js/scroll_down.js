a859636daf79661baaacd78ff4fe62c2cb6a1c33
maiiitian <mugibatake99@gmail.com>
2020-10-11 12:42:19 +0800

    second commit

--- js/scroll_down.js
+++ js/scroll_down.js
@@ -1,19 +1,19 @@
-$('div.scroll-down a').on('click',function(e){
-    if(this.hash !== ''){
-        e.preventDefault();
+// $('div.scroll-down a').on('click',function(e){
+//     if(this.hash !== ''){
+//         e.preventDefault();
 
-        const hash = this.hash;
+//         const hash = this.hash;
 
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        },
-        "slow");
-    }
-});
-
-// $(document).ready(function ($) {
-    // $(".scroll-down").click(function (event) {
-    //     event.preventDefault();
-    //     $('html,body').animate({scrollTop: $(this.hash).offset().top}, "slow");
-    // });
+//         $('html, body').animate({
+//             scrollTop: $(hash).offset().top
+//         },
+//         "slow");
+//     }
 // });
+
+$(document).ready(function ($) {
+    $('div.scroll-down a').click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({scrollTop: $(this.hash).offset().top}, "slow");
+    });
+});
